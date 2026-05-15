49763f877e40b0ffbc1b56aaf33de223b3c85b4a
Hejn <2274920871@qq.com>
2026-05-15 14:49:41 +0800

    init NotionNext

---
+++ pages/[prefix]/[slug]/[...suffix].js
@@ -0,0 +1,63 @@
+import BLOG from '@/blog.config'
+import { siteConfig } from '@/lib/config'
+import { resolvePostProps } from '@/lib/db/SiteDataApi'
+import { checkSlugHasMorThanTwoSlash } from '@/lib/utils/post'
+import Slug from '..'
+import { getStaticPathsBase } from '@/lib/build/staticPaths'
+
+/**
+ * 根据notion的slug访问页面
+ * 解析三级以上目录 /article/2023/10/29/test
+ * @param {*} props
+ * @returns
+ */
+const PrefixSlug = props => {
+  return <Slug {...props} />
+}
+
+
+export async function getStaticPaths() {
+  return getStaticPathsBase({
+    from: 'slug-paths',
+    filterFn: row => checkSlugHasMorThanTwoSlash(row),
+    mapPageToParams: row => ({
+      params: {
+        prefix: row.slug.split('/')[0],
+        slug: row.slug.split('/')[1],
+        suffix: row.slug.split('/').slice(2)
+      }
+    })
+  })
+}
+
+/**
+ * 抓取页面数据
+ * @param {*} param0
+ * @returns
+ */
+export async function getStaticProps({
+  params: { prefix, slug, suffix },
+  locale
+}) {
+
+  const props = await resolvePostProps({
+    prefix,
+    slug,
+    suffix,
+    locale,
+  })
+
+  return {
+    props,
+    revalidate: isExport()
+      ? undefined
+      : siteConfig(
+        'NEXT_REVALIDATE_SECOND',
+        BLOG.NEXT_REVALIDATE_SECOND,
+        props.NOTION_CONFIG
+      ),
+    notFound: !props.post
+  }
+}
+
+export default PrefixSlug
---
+++ pages/[prefix]/[slug]/index.js
@@ -0,0 +1,51 @@
+import BLOG from '@/blog.config'
+import { siteConfig } from '@/lib/config'
+import { resolvePostProps } from '@/lib/db/SiteDataApi'
+import Slug from '..'
+import { checkSlugHasOneSlash } from '@/lib/utils/post'
+import { getStaticPathsBase } from '@/lib/build/staticPaths'
+
+/**
+ * 根据notion的slug访问页面
+ * 解析二级目录 /article/about
+ * @param {*} props
+ * @returns
+ */
+const PrefixSlug = props => {
+  return <Slug {...props} />
+}
+
+export async function getStaticPaths() {
+  return getStaticPathsBase({
+    from: 'slug-paths',
+    filterFn: row => checkSlugHasOneSlash(row),
+    mapPageToParams: row => ({
+      params: {
+        prefix: row.slug.split('/')[0],
+        slug: row.slug.split('/')[1]
+      }
+    })
+  })
+}
+
+export async function getStaticProps({ params: { prefix, slug }, locale }) {
+  const props = await resolvePostProps({
+    prefix,
+    slug,
+    locale,
+  })
+
+  return {
+    props,
+    revalidate: isExport()
+      ? undefined
+      : siteConfig(
+        'NEXT_REVALIDATE_SECOND',
+        BLOG.NEXT_REVALIDATE_SECOND,
+        props.NOTION_CONFIG
+      ),
+    notFound: !props.post
+  }
+}
+
+export default PrefixSlug
